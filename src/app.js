5a03b2e671e6d51d544065e45b7a974a9ebe7f33
Petr Krůček <petr.krucek@firstlinesoftware.com>
2019-09-27 10:15:31 +0200

    comment additional code

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ const store = configureStore();
 // const state = store.getState();
 // const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-console.log(visibleExpenses);
+// console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
